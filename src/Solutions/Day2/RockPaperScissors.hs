523e7da9a979dde88b04c9e4770e97a510c78567
cdimitroulas <c.dimitroulas@gmail.com>
2022-12-02 18:43:36 +0000

    Refactoring Day2

--- src/Solutions/Day2/RockPaperScissors.hs
+++ src/Solutions/Day2/RockPaperScissors.hs
@@ -1,6 +1,5 @@
 module Solutions.Day2.RockPaperScissors (solution_1) where
 
-import           Control.Monad        ((>=>))
 import           Data.Attoparsec.Text (Parser)
 import qualified Data.Attoparsec.Text as Parser
 import           Data.Text            (Text)
@@ -12,17 +11,6 @@ data GameResult = Win | Lose | Draw deriving (Show)
 
 data GameStrategy = GameStrategy { opponentChoice :: GameChoice, yourChoice :: GameChoice }
 
--- Given a game choice, what is the correct choice in order to win?
-winningChoice :: GameChoice -> GameChoice
-winningChoice Scissors   = Rock
-winningChoice gameChoice = succ gameChoice
-
-playGame :: GameStrategy -> GameResult
-playGame GameStrategy{..}
-  | opponentChoice == yourChoice = Draw
-  | winningChoice opponentChoice == yourChoice = Win
-  | otherwise = Lose
-
 choiceScore :: GameChoice -> Int
 choiceScore Rock     = 1
 choiceScore Paper    = 2
@@ -33,14 +21,26 @@ resultScore Win  = 6
 resultScore Draw = 3
 resultScore Lose = 0
 
-calcScore :: GameStrategy -> Int
-calcScore strategy@GameStrategy{..} = resultScore (playGame strategy) + choiceScore yourChoice
+-- Given a game choice, what is the correct choice in order to win?
+winningChoice :: GameChoice -> GameChoice
+winningChoice Scissors   = Rock
+-- The GameChoices are written in order so that the successor wins against it's predecessor
+winningChoice gameChoice = succ gameChoice
 
-solve_1 :: [GameStrategy] -> Int
-solve_1 = foldl (\total strategy -> total + calcScore strategy) 0
+-- Returns the game result given a game strategy
+strategyResult :: GameStrategy -> GameResult
+strategyResult GameStrategy{..}
+  | opponentChoice == yourChoice = Draw
+  | winningChoice opponentChoice == yourChoice = Win
+  | otherwise = Lose
 
 solution_1 :: IO (Maybe Int)
-solution_1 = solve "data/day2.txt" parser solve_1
+solution_1 = solve "data/day2.txt" parser (sum . map calcScore)
+  where
+    parser = mapM parseGameStrategy . filter (/= "")
+    calcScore strategy@GameStrategy{..} =
+      resultScore (strategyResult strategy) + choiceScore yourChoice
+
 
 data GameStrategy2 = GameStrategy2 { opponentChoice2 :: GameChoice, desiredResult :: GameResult }
 
@@ -50,32 +50,20 @@ parseResult 'Y' = Just Draw
 parseResult 'Z' = Just Win
 parseResult _   = Nothing
 
-parseGameStrategy2 :: (Char, Char) -> Maybe GameStrategy2
-parseGameStrategy2 (opponentLetter, resultLetter) =
-  GameStrategy2 <$> parseChoice opponentLetter <*> parseResult resultLetter
-
-parser2 :: [Text] -> Maybe [GameStrategy2]
-parser2 = mapM _parseLine . filter (/= "")
-  where
-    _parseLine = Parser.maybeResult . Parser.parse parseLine >=> parseGameStrategy2
-
 -- Calculates what game choice you need to make to get the desired result
-calcChoice :: GameStrategy2 -> GameChoice
-calcChoice GameStrategy2{ opponentChoice2, desiredResult = Draw } = opponentChoice2
-calcChoice GameStrategy2{ opponentChoice2, desiredResult = Lose } =
+choiceForDesiredResult :: GameStrategy2 -> GameChoice
+choiceForDesiredResult GameStrategy2{ opponentChoice2, desiredResult = Draw } = opponentChoice2
+choiceForDesiredResult GameStrategy2{ opponentChoice2, desiredResult = Lose } =
   winningChoice $ winningChoice opponentChoice2
-calcChoice GameStrategy2{ opponentChoice2, desiredResult = Win } =
+choiceForDesiredResult GameStrategy2{ opponentChoice2, desiredResult = Win } =
   winningChoice opponentChoice2
 
-calcScore2 :: GameStrategy2 -> Int
-calcScore2 strategy@GameStrategy2{..} =
-  resultScore desiredResult + choiceScore (calcChoice strategy)
-
-solve_2 :: [GameStrategy2] -> Int
-solve_2 = foldl (\total strategy -> total + calcScore2 strategy) 0
-
 solution_2 :: IO (Maybe Int)
-solution_2 = solve "data/day2.txt" parser2 solve_2
+solution_2 = solve "data/day2.txt" parser (sum . map calcScore)
+  where
+    parser = mapM parseGameStrategy2 . filter (/= "")
+    calcScore strategy@GameStrategy2{..} =
+      resultScore desiredResult + choiceScore (choiceForDesiredResult strategy)
 
 ---- Parsing
 --
@@ -89,18 +77,19 @@ parseChoice 'Y' = Just Paper
 parseChoice 'Z' = Just Scissors
 parseChoice _   = Nothing
 
-parseGameStrategy :: (Char, Char) -> Maybe GameStrategy
-parseGameStrategy (opponentLetter, yourLetter) =
+lineParser :: Parser (Char, Char)
+lineParser = do
+    opponentLetter <- Parser.letter
+    Parser.space
+    yourLetter <- Parser.letter
+    return (opponentLetter, yourLetter)
+
+parseGameStrategy :: Text -> Maybe GameStrategy
+parseGameStrategy line = do
+  (opponentLetter, yourLetter) <- Parser.maybeResult . Parser.parse lineParser $ line
   GameStrategy <$> parseChoice opponentLetter <*> parseChoice yourLetter
 
-parseLine :: Parser (Char, Char)
-parseLine = do
-  opponentLetter <- Parser.letter
-  Parser.space
-  yourLetter <- Parser.letter
-  return (opponentLetter, yourLetter)
-
-parser :: [Text] -> Maybe [GameStrategy]
-parser = mapM _parseLine . filter (/= "")
-  where
-    _parseLine = Parser.maybeResult . Parser.parse parseLine >=> parseGameStrategy
+parseGameStrategy2 :: Text -> Maybe GameStrategy2
+parseGameStrategy2 line = do
+  (opponentLetter, resultLetter) <- Parser.maybeResult . Parser.parse lineParser $ line
+  GameStrategy2 <$> parseChoice opponentLetter <*> parseResult resultLetter
